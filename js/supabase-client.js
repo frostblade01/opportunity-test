79523180b771bc694bcac344c340fa2e2f8b83fb
A96 <90542458+Aaron9306@users.noreply.github.com>
2026-08-08 19:12:34 +0400

    messed up url

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = 'https://zctmbbznaijyunlycftu.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://zctmbbznaijyunlycftu.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_KSq9kfjKLmeg-n1s9OPo0Q_Oe5YXleS';
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
