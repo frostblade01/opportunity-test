1a7d054c34ed39428b91e236015a92deecb1004b
A96 <90542458+Aaron9306@users.noreply.github.com>
2026-08-08 18:38:34 +0400

    Remove event listener for seeMoreBtn as not required

--- js/home.js
+++ js/home.js
@@ -1,14 +1,2 @@
-﻿
 document.addEventListener('DOMContentLoaded', () => {
-  const seeMoreBtn = document.getElementById('seeMoreBtn');
-  if(seeMoreBtn){
-    seeMoreBtn.addEventListener('click', (e) => {
-      if(!getCurrentUser()){
-        e.preventDefault();
-        window.location.href = 'login.html';
-      }
-
-    });
-  }
 });
-
